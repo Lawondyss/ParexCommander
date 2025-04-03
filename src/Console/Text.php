955af04924ad6af4cb5492f84d477da9d7b54e3d
Ladislav Vondráček <lawondyss@gmail.com>
2025-04-03 19:17:53 +0200

    Added console component for text input

---
+++ src/Console/Text.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Lawondyss\ParexCommander\Console;
+
+use function fgets;
+use function is_string;
+use function trim;
+
+use const STDIN;
+
+class Text
+{
+  public function make(
+    string $prompt,
+    string $default = '',
+    ?callable $validator = null,
+  ): string {
+    while (true) {
+      $displayPrompt = $prompt;
+      $default && $displayPrompt .= " [{$default}]";
+      echo $displayPrompt, ' ';
+
+      $text = trim(fgets(STDIN));
+      $text = $text !== '' ? $text : $default;
+
+      if ($validator !== null) {
+        $result = $validator($text);
+
+        if ($result !== true) {
+          $error = is_string($result) ? $result : 'Invalid value';
+          echo $error, PHP_EOL;
+          continue;
+        }
+      }
+
+      return $text;
+    }
+  }
+}
